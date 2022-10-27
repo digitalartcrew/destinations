744daebaf5d5f2fbadacfd08ed0365c6403db01a
Donte Burney <donteburney@Dontes-Mac-mini.local>
2022-10-27 09:12:31 -1000

    added passport

--- routes/auth-router.js
+++ routes/auth-router.js
@@ -1,33 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const { User } = require("./../models");
-const db = require("../db");
-
-passport.use(
-  "local-signup",
-  new LocalStrategy(
-    {
-      username: "username",
-      password: "password",
-    },
-    async (username, password, done) => {
-      try {
-        // check if user exists
-        const userExists = await User.findOne({ username: username });
-        if (userExists) {
-          return done(null, false);
-        }
-        // Create a new user with the user data provided
-        const user = await User.create({ username, password });
-        return done(null, user);
-      } catch (error) {
-        done(error);
-      }
-    }
-  )
-);
 
 router.post(
   "/login",
