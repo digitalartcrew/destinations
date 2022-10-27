42803f38aa59fa93d4add9ee1400648ac35a3e45
Donte Burney <donteburney@Dontes-Mac-mini.local>
2022-10-27 12:52:51 -1000

    added component structure

--- client/src/components/EntityCards.js
+++ client/src/components/EntityCards.js
@@ -1,21 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import EntityCard from "./EntityCard";
 
 const EntityCards = () => {
+  const [destinations, setDestinations] = useState([]);
+  // make data request
+  useEffect(() => {
+    fetch("/api/destination")
+      .then((res) => res.json())
+      .then((data) => setDestinations(data));
+  });
+
   return (
     <div className="row">
-      <EntityCard
-        title="Title 1"
-        imageUrl="https://a.cdn-hotels.com/gdcs/production121/d137/d4030d17-700f-4fd1-9c05-4b1742c7a6c3.jpg?impolicy=fcrop&w=800&h=533&q=medium"
-      />
-      <EntityCard
-        title="Title 1"
-        imageUrl="https://a.cdn-hotels.com/gdcs/production121/d137/d4030d17-700f-4fd1-9c05-4b1742c7a6c3.jpg?impolicy=fcrop&w=800&h=533&q=medium"
-      />
-      <EntityCard
-        title="Title 1"
-        imageUrl="https://a.cdn-hotels.com/gdcs/production121/d137/d4030d17-700f-4fd1-9c05-4b1742c7a6c3.jpg?impolicy=fcrop&w=800&h=533&q=medium"
-      />
+      {destinations.map((destination) => (
+        <EntityCard title={destination.title} />
+      ))}
     </div>
   );
 };
