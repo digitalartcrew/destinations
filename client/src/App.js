6d2aa4fa1c59a54cf9ceb50dd3d4c292f8e92a14
Donte Burney <donteburney@Dontes-Mac-mini.local>
2022-10-27 12:41:37 -1000

    added header

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,17 @@ import EntityCards from "./components/EntityCards";
 function App() {
   return (
     <div className="container">
-      <header></header>
+      <header>
+        <div class="p-5 mb-4 bg-light rounded-3">
+          <div class="container-fluid text-center py-5">
+            <h1>Vacations Destination Wish List</h1>
+            <p>
+              Found exciting places to visition on your next vacation? Use the
+              form below to add them to your wishlist
+            </p>
+          </div>
+        </div>
+      </header>
       <main>
         <div className="row">
           <div className="col-md-3">
